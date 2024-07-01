6823c27e404d27732e27637bdea0fa8d3b7866ac
MarySnik <mary.snikk@gmail.com>
2024-07-01 12:53:14 +0400

    finally figured out menu js

--- js/menu/dishes-data.js
+++ js/menu/dishes-data.js
@@ -1,29 +1,39 @@
 // menu. the best way is databases
 // can add currency
 // also can do htat
-// for us admins creating a new dish
 class Dish { 
-  constructor(name, price, portion, description, category){
+  constructor(name, price, portion, category, description){
     this.name = name;
     this.price = price;
     this.portion = portion
-    this.description = description;
     this.category = category
+    this.description = description;
   }
 }
 
-
 // side
-let friedMushrooms = new Dish('Fried mushrooms', 350, 150, '', 'side dish');
+let side01 = new Dish('Fried mushrooms', 350, 150, 'side dish');
 
-let boiledPotato = new Dish('Boiled potatoes with dill, butter and garlic', 200, 150, '', 'side dish');
+let side02 = new Dish('Boiled potatoes with dill, butter and garlic', 200, 150, 'side dish');
 
-let grilledVeg = new Dish('Grilled vegetables', 350, 150, '', 'side dish');
+let side03 = new Dish('Grilled vegetables', 350, 150, 'side dish');
 
 // pasta
+let pasta01 = new Dish('Sphagetti with bolognese sause', 690, 290, 'pasta', 'Sphagetti with tomato-meat sauce');
+let pasta02 = new Dish('Penne with chicken and creamy mushroom sauce', 590, 270, 'pasta');
+let pasta03 = ("Penne with arabyata sauce", 490, 280, 'pasta', 'Penne with vegetable sauce arabiata');
+let pasta04 = new Dish('Talglatelle with carbonara sauce', 690, 300, 'pasta', 'Classic Roman pasta tagliatelle with bacon in parmesan egg yolk sauce') 
+let pasta05 = new Dish('Tagliatelle with salmon and creamy spinach sauce', 690, 250, 'pasta') 
 
-let sphaghettiBalagnese = new Dish('Sphagetti with bolognese sause', 690, 290, 'Sphagetti with tomato-meat sauce', 'pasta');
-let penneChicken = new Dish('Penne with chicken and creamy mushroom sauce', 590, 270, category='pasta');
-let penneArab = ("Penne with arabyata sauce", 490, 280, 'Penne with vegetable sauce arabiata', 'pasta');
-let tagliatelleCarb = new Dish('Talglatelle with carbonara sauce', 690, 300, 'Classic Roman pasta tagliatelle with bacon in parmesan egg yolk sauce', 'pasta') 
-let tagaliatelleSalmon = new Dish('Tagliatelle with salmon and creamy spinach sauce', 690, 250, category='pasta') 
+// breakfast
+let breakfast01 = new Dish('Crepes with jam / sour cream', 220, 160, 'breakfast')
+
+let breakfast02 = new Dish('Crepes with ham and cheese', 350, 160, 'breakfast')
+
+let breakfast03 = new Dish('Sandwich with ham and cheese', 350, 250, 'breakfast')
+
+let breakfast04 = new Dish('Sandwich with chiekcen', 350, 250, 'breakfast')
+
+let breakfast05 = new Dish('Cottage cheese', 350, 250, 'breakfast')
+
+export let breakfast = [breakfast01, breakfast02, breakfast03, breakfast04, breakfast05];
--- js/menu/menu.js
+++ js/menu/menu.js
@@ -1,35 +1,36 @@
-let breakfast = [];
+import {breakfast} from './dishes-data.js'
 // add menus from js file dishes
-let mainMenu = [];
-let drinks = []
-let pasta = [sphaghettiBalagnese, penneChicken, penneArab, tagaliatelleSalmon, tagliatelleCarb]
 
-let menuItems = document.querySelectorAll('.menuCat')
+let menuText = document.querySelector('.menuTable')
+let category = document.querySelector('.menuCat').classList[1]; // breakfast
 
-menuItems.forEach((item) => {
-  item.addEventListener('click', function() {
-    showMenu(item.innerText)});
-})
-
-function showMenu(category){
-  console.log('works')
-  console.log(typeOf(category))
-  category.forEach((element) => {
-    const li = document.createElement('li');
-    // format flex on the line
-    // name - - - - - - - 1$
-    li.textContent = `
-    <div class='mainContent'>
-    <p>${element.name}</p>
-    <span>${element.portion}</span>
-    <p>${element.price}</p>
-    </div>
-    <div class='description'>
-    <span>${element.description}</span>
-    </div>
-    `
-  
-    document.querySelector('.menuUl').appendChild(li)
-    })
-
+switch (category){
+  case 'breakfast':
+    showMenu(breakfast);
+    break;
+  case 'pasta':
+    showMenu(pasta);
+    break;
 }
+
+let categoryMenu = '';
+
+function showMenu(thisCategory) {
+
+
+  document.querySelector('.menu h2').innerHTML = category;
+  
+  thisCategory.forEach((dish) => {
+    console.log(dish)
+    
+    let categoryMenu = `
+    <div class='position>
+    <div class='upper-info'>
+    <h6 class='name'>${dish.name}</h6>
+    <h6 class='price'>${dish.price}</h6>
+    </div>
+    </div>    
+    `
+    menuText.innerHTML += categoryMenu;
+  })}
+  
